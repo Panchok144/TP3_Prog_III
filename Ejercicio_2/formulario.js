7956c6a0ed07fbf0d5899d290002a7ef5420b004
Castro Francisco <franmercadoc16@gmail.com>
2025-06-25 21:36:06 -0300

    Agregando estilo y entendiendo mejor el ejercicio

--- Ejercicio_2/formulario.js
+++ Ejercicio_2/formulario.js
@@ -1,30 +1,32 @@
-
 const boton = document.getElementById("Boton");
 
-function Filtrar() {
+boton.addEventListener("click", function() {
 
   const palabra = document.getElementById("ingresar").value.trim().toLowerCase();
-
   const resultado = document.getElementById("resultado");
 
   const palabras_cargadas = ["manzana", "banana", "pera", "durazno", "frutilla", "mango"];
-
-  
 
   if (palabra === "") {
     resultado.innerHTML = `<p>ERROR: no se ingresó una palabra</p>`;
     return;
   }
 
+  
   const PF = palabras_cargadas.filter(p => p.toLowerCase().includes(palabra));
+
+  
+  
 
   if (PF.length > 0) {
     const NP = PF.map(f => `<li>${f}</li>`).join("");
-    resultado.innerHTML = `<ul>${NP}</ul>`;
+    resultado.innerHTML = 
+    ` 
+      <p> Palabras similar: ${palabra}  
+      <ul>${NP}</ul>
+      `;
   } else {
     resultado.innerHTML = `<p>No se encontraron coincidencias</p>`;
   }
-}
+});
 
-
-boton.addEventListener("click", Filtrar);
