2b1b706ca92e3ed6b8fa441acf5e5934ca802349
Castro Francisco <franmercadoc16@gmail.com>
2025-06-28 19:00:06 -0300

    Modificando linea 22 Operaciones.js

--- Ejercicio_1/Operaciones.js
+++ Ejercicio_1/Operaciones.js
@@ -5,7 +5,23 @@ const boton = document.getElementById("calcular")
 const resultado = document.getElementById("resultado")
 
 
+function verificarDivision(){
 
+     const n1 = parseFloat(numero1.value);
+  const n2 = parseFloat(numero2.value);
+
+  if (Operaciones.value === "division" && (n1 === 0 || n2 === 0)) {
+    boton.disabled = true;
+    resultado.textContent = "Error: no se puede dividir por cero";
+  } else {
+    boton.disabled = false;
+   
+  }
+}
+
+numero1.addEventListener("input", verificarDivision);
+numero2.addEventListener("input", verificarDivision);
+Operaciones.addEventListener("change", verificarDivision);
 
 boton.addEventListener("click", function () {
 
@@ -32,14 +48,10 @@ boton.addEventListener("click", function () {
 
         case "division":
 
-        if (numero_1 === 0 || numero_2 === 0){
-
-             resultado.textContent = "Error: no se puede dividir por cero";
-             return;
-        }else {
-
+       
             resul = numero_1 / numero_2;
-        }
+            
+      break;
 
     }
 
